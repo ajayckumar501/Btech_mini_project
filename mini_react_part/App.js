cf8dd01f71759e8673c0ac2e761646fde43ce7c9
ajayckumar501 <ajayckumar456@gmail.com>
2024-07-01 15:02:53 +0530

    modified

--- mini_react_part/App.js
+++ mini_react_part/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation'; // Assuming navigation.js is in the same folder
 import { UserProvider } from './context/userdataContext';
+import { StatusBar } from 'react-native';
+import { enableScreens } from 'react-native-screens';
+enableScreens();
+
 
 export default function App() {
   return (
-    <UserProvider>
-       <NavigationContainer>
-         <AppNavigator />
-       </NavigationContainer>
-    </UserProvider>
+    <>
+      {/* Hide the status bar */}
+      <StatusBar hidden={true} />
+      <UserProvider>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </UserProvider>
+    </>
   );
 }
